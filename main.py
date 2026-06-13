4ac85625811533d4bd88da695b4966f745959fa1
PIYUSH SONI 🕸 <piyushsoni7777777@gmail.com>
2026-06-13 15:01:21 +0530

    Add comment before parsing HTML response
    Added a comment for clarity before parsing the website HTML.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ date = input("Which day do you want to travel to? Type the date in this format: 
 URL = f"https://www.billboard.com/charts/hot-100/{date}/"
 response = requests.get(URL)
 website_html = response.text
-
+# website
 soup = BeautifulSoup(website_html, "html.parser")
 
 all_songs = soup.findAll(name="h3", class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only", id="title-of-a-story")
