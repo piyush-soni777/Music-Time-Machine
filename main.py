a7a4183ae712a990f676abb8a067bfc17f4e85d6
PIYUSH SONI 🕸 <piyushsoni7777777@gmail.com>
2026-06-13 15:02:11 +0530

    Extract song titles from website HTML

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ response = requests.get(URL)
 website_html = response.text
 # website
 soup = BeautifulSoup(website_html, "html.parser")
-
+# finding songs
 all_songs = soup.findAll(name="h3", class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only", id="title-of-a-story")
 number_one = soup.find(name="h3", class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 u-font-size-23@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-245 u-max-width-230@tablet-only u-letter-spacing-0028@tablet", id="title-of-a-story").getText()
 number_one = number_one.replace("\n", "")
